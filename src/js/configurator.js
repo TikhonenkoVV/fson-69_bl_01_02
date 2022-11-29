142ea16a8c91808ef21fc1cf6fd351914b86f00d
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2022-11-29 18:06:55 +0200

    add img arch

--- src/js/configurator.js
+++ src/js/configurator.js
@@ -1,33 +1,75 @@
 (() => {
-  const labelHoverS = document.querySelector('.configurator__label--smaller');
-  const labelHoverM = document.querySelector('.configurator__label--medium');
-  const labelHoverL = document.querySelector('.configurator__label--larger');
-  // const chkBoxS = document.querySelector('.configurator__checkbox--smaller');
-  // const chkBoxM = document.querySelector('.configurator__checkbox--smaller');
-  // const chkBoxL = document.querySelector('.configurator__checkbox--smaller');
+  const group = document.querySelector('.configurator__group');
+  const chkboxS = document.querySelector('.configurator__checkbox--smaller');
+  const chkboxM = document.querySelector('.configurator__checkbox--medium');
+  const chkboxL = document.querySelector('.configurator__checkbox--larger');
   const imgBgr = document.querySelector('.configurator__img');
+  const pathA =
+    'https://raw.githubusercontent.com/TikhonenkoVV/prj-arch/main/img/';
+  const srcA = imgBgr.src;
+  const srcB = '';
+  const srcC = '';
+  const srcD = '';
 
-  labelHoverS.onmouseenter = function () {
-    imgBgr.classList.toggle('smaller-bgr-img');
-  };
+  chkboxS.addEventListener('change', event => {
+    imgBgr.src = srcB;
+    srcA = srcB;
+    console.log(srcA);
+  });
 
-  labelHoverS.onmouseleave = function () {
-    imgBgr.classList.toggle('smaller-bgr-img');
-  };
+  chkboxM.addEventListener('change', event => {
+    imgBgr.src = srcC;
+    srcA = srcC;
+  });
 
-  labelHoverM.onmouseenter = function () {
-    imgBgr.classList.toggle('medium-bgr-img');
-  };
+  chkboxL.addEventListener('change', event => {
+    imgBgr.src = srcD;
+    srcA = srcD;
+  });
+  // group.addEventListener('click', event => {
+  //   if (chkboxS.checked) {
+  //     imgBgr.classList.toggle('smaller-bgr-img-chk');
+  //   }
+  //   if (chkboxM.checked) {
+  //     imgBgr.classList.toggle('medium-bgr-img-chk');
+  //   }
+  //   if (chkboxL.checked) {
+  //     imgBgr.classList.toggle('larger-bgr-img-chk');
+  //   }
+  // });
 
-  labelHoverM.onmouseleave = function () {
-    imgBgr.classList.toggle('medium-bgr-img');
-  };
+  group.addEventListener('mouseover', event => {
+    if (event.target.classList.contains('configurator__label--smaller')) {
+      const strA = 'home-1.png';
+      const strB = pathA + strA;
+      imgBgr.src = strB;
+      srcB = strB;
+    } else {
+      if (event.target.classList.contains('configurator__label--medium')) {
+        srcC = imgBgr.src = pathA + 'home-2.png';
+      } else {
+        if (event.target.classList.contains('configurator__label--larger')) {
+          srcD = imgBgr.src = pathA + 'home-3.png';
+        }
+      }
+    }
+  });
 
-  labelHoverL.onmouseenter = function () {
-    imgBgr.classList.toggle('larger-bgr-img');
-  };
+  group.addEventListener('mouseout', event => {
+    imgBgr.src = srcA;
+  });
 
-  labelHoverL.onmouseleave = function () {
-    imgBgr.classList.toggle('larger-bgr-img');
-  };
+  // group.addEventListener('mouseout', event => {
+  //   if (event.target.classList.contains('configurator__label--smaller')) {
+  //     imgBgr.src = pathB;
+  //   } else {
+  //     if (event.target.classList.contains('configurator__label--medium')) {
+  //       imgBgr.src = './images/home-2.png';
+  //     } else {
+  //       if (event.target.classList.contains('configurator__label--larger')) {
+  //         imgBgr.src = './images/home-3.png';
+  //       }
+  //     }
+  //   }
+  // });
 })();
